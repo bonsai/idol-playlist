95afc2344aece786bc31a97706fa424f14256729
bonsai <onsen.bonsai@gmail.com>
2026-09-18 16:53:51 +0900

    feat: expose Django admin

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
+from django.contrib import admin
 from django.urls import path
+
 from catalog.views import dashboard
 
 urlpatterns = [
     path("", dashboard, name="dashboard"),
+    path("admin/", admin.site.urls),
 ]
